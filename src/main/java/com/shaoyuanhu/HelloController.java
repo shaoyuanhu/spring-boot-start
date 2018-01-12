332fcab82f53b9b948df0df1dadf2cc457877934
shaoyh2 <shaoyh2@lenovo.com>
2018-01-12 14:33:26 +0800

    controller的使用以及thymeleaf模本视图

--- src/main/java/com/shaoyuanhu/HelloController.java
+++ src/main/java/com/shaoyuanhu/HelloController.java
@@ -1,16 +1,17 @@
 package com.shaoyuanhu;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description:
  * @Author: ShaoYuanHu
  * @Date: 2018/1/11
  */
-@RestController
+//@RestController
+@Controller
+@RequestMapping("/he")
 public class HelloController {
 
     /*@Value("${cupSize}")
@@ -25,10 +26,17 @@ public class HelloController {
     @Autowired
     private GirlProperties girlProperties;
 
-    @RequestMapping(value = "/hello", produces = "application/json; charset=utf-8")
-    public String hello(){
+    //@RequestMapping(value = {"/hello","/hi"}, method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    //@RequestMapping(value = "/hello/{name}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+    //@GetMapping(value = "/hello",produces = "application/json; charset=utf-8")
+    @GetMapping(value = "/hello",produces = "text/html; charset=utf-8")
+    @ResponseBody
+    //public String hello(@PathVariable("name")String name){
+    public String hello(@RequestParam(value = "name",required = true,defaultValue = "xiaohei")String name){
+    //public String hello(String name){
 //        return "Hello Spring Boot!! " + cupSize + "\t" + age;
-        return girlProperties.getCupSize()+"--"+girlProperties.getAge();
+//        return girlProperties.getCupSize()+"--"+girlProperties.getAge();
+        return name;
     }
 
 }
--- src/test/java/com/shaoyuanhu/SpringBootStartApplicationTests.java
+++ src/test/java/com/shaoyuanhu/SpringBootStartApplicationTests.java
@@ -1,16 +1,64 @@
 package com.shaoyuanhu;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootStartApplicationTests {
 
+	@Autowired
+	private GirlProperties girlProperties;
+
+	@Before
+	public void before(){
+		System.out.println("this is before method");
+	}
+
 	@Test
 	public void contextLoads() {
+		System.out.println(girlProperties.getCupSize());
+
+	}
+
+	@Test
+	public void run(){
+		try {
+			String className = "com.shaoyuanhu.GirlProperties";
+			Class c = Class.forName(className);
+			System.out.println("--------------------------");
+			Field[] fields = c.getDeclaredFields();
+			for (Field field : fields) {
+				System.out.println(field);
+			}
+			System.out.println("--------------------------");
+			Constructor[] constructors = c.getDeclaredConstructors();
+			for (Constructor constructor : constructors) {
+				System.out.println(constructor);
+			}
+			System.out.println("--------------------------");
+			Method[] methods = c.getDeclaredMethods();
+			for (Method method : methods) {
+				System.out.println(method);
+			}
+			System.out.println("--------------------------");
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	@After
+	public void after(){
+		System.out.println("this is after method");
 	}
 
 }
